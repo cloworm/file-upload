6dd675651a7f627838556090dddb755693632257
Chloe Echikson <Chloe.Echikson@gmail.com>
2021-02-12 13:37:56 -0500

    can drag and drop multiple files into drop zone and view them in a list

--- force-app/main/default/lwc/fileUpload/fileUpload.js
+++ force-app/main/default/lwc/fileUpload/fileUpload.js
@@ -1,31 +1,50 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, track } from "lwc";
 import uploadFile from "@salesforce/apex/FileUploadController.uploadFile";
 
 export default class FileUpload extends LightningElement {
   @api recordId;
   @api prop1;
   acceptedFileTypes = "image/*";
-  allowMultiple = false;
-  fileData;
+  allowMultiple = true;
+  @track files = [
+    {
+      id: this.uniqueID(),
+      filename: "Test.csv",
+      base64: "test",
+      recordId: this.recordId,
+      type: "text/csv"
+    }
+  ];
+  isDragging = false;
 
   handleInputChange(event) {
-    this.fileData = null;
+    this.files = [];
 
-    const file = event.target.files[0];
-    console.log("files", file);
+    event.target.files.forEach((file) => this.processFile(file));
+  }
+
+  get hasFiles() {
+    console.log("hasFiles", this.files && this.files.length > 0);
+    return this.files && this.files.length > 0;
+  }
+
+  processFile(file) {
+    console.log("processing file!", file);
 
     const reader = new FileReader();
     reader.onload = () => {
       var base64 = reader.result.split(",")[1];
-      this.fileData = {
+      const fileData = {
+        id: this.uniqueID(),
         filename: file.name,
         base64: base64,
         recordId: this.recordId,
         type: file.type
       };
-      console.log(this.fileData);
+      this.files.push(fileData);
+      console.log(fileData);
 
-      this.handleUpload(this.fileData);
+      this.handleUpload(fileData);
     };
     reader.readAsDataURL(file);
   }
@@ -40,13 +59,74 @@ export default class FileUpload extends LightningElement {
       });
   }
 
+  handleDragEnter(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (!this.isDragging) {
+      console.log("animation start");
+      const dropzone = this.template.querySelector("[data-id=dropzone]");
+      dropzone.classList.add("box-animate");
+      console.log("added box-animate", dropzone.classList);
+    }
+
+    this.isDragging = true;
+  }
+
+  handleDragLeave(event) {
+    console.log("drag leave");
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  handleDrop(event) {
+    console.log("dropped!");
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("files", ...event.dataTransfer.files);
+
+    const dropzone = this.template.querySelector("[data-id=dropzone]");
+    // this.log("dropzone", dropzone);
+    dropzone.classList.remove("box-animate");
+    console.log("removed drag", dropzone.classList);
+    this.isDragging = false;
+    console.log("animation end");
+    console.log("files type", typeof event.dataTransfer.files);
+    [...event.dataTransfer.files].forEach((file) => {
+      console.log("file...", file);
+      this.processFile(file);
+    });
+  }
+
   get iconName() {
     switch (this.fileData.type) {
       case "image/png":
         return "doctype:image";
 
+      case "text/csv":
+        return "doctype:csv";
+
+      case "application/vnd.ms-excel":
+        return "doctype:excel";
+
+      case "application/pdf":
+        return "doctype:pdf";
+
+      case "application/vnd.ms-powerpoint":
+        return "doctype:ppt";
+
+      case "application/zip":
+        return "doctype:zip";
+
       default:
         return "doctype:unknown";
     }
+  }
+
+  uniqueID() {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return "_" + Math.random().toString(36).substr(2, 9);
   }
 }
