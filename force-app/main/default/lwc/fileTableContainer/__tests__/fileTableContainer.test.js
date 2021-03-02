3ba4a146846da7c4387c9b88ec4e8d2013cc2eb6
Chloe Echikson <Chloe.Echikson@gmail.com>
2021-03-02 15:37:26 -0500

    Testing select component

--- force-app/main/default/lwc/fileTableContainer/__tests__/fileTableContainer.test.js
+++ force-app/main/default/lwc/fileTableContainer/__tests__/fileTableContainer.test.js
@@ -1,5 +1,5 @@
 import { createElement } from "lwc";
-import FileGridContainer from "c/fileTableContainer";
+import FileTableContainer from "c/fileTableContainer";
 import getFiles from "@salesforce/apex/FileTableController.getFiles";
 const mockGetFiles = require("./data/getFiles.json");
 import { registerLdsTestWireAdapter } from "@salesforce/sfdx-lwc-jest";
@@ -15,7 +15,7 @@ describe("c-file-table-container", () => {
 
   it("Displays the file count in a header tag when there are no files", () => {
     const element = createElement("c-file-table-container", {
-      is: FileGridContainer
+      is: FileTableContainer
     });
     element.recordId = "12345";
     document.body.appendChild(element);
@@ -24,9 +24,9 @@ describe("c-file-table-container", () => {
     expect(message.textContent).toContain("All Files (0)");
   });
 
-  it("Displays the file count in a header tag when there are 2 files", () => {
+  it("Displays a file count of 2 if getFiles returns 2 files", () => {
     const element = createElement("c-file-table-container", {
-      is: FileGridContainer
+      is: FileTableContainer
     });
     element.recordId = "12345";
     document.body.appendChild(element);
@@ -36,6 +36,28 @@ describe("c-file-table-container", () => {
     return Promise.resolve().then(() => {
       const message = element.shadowRoot.querySelector("h2");
       expect(message.textContent).toContain("All Files (2)");
+    });
+  });
+
+  it("Will create 2 accordion sections if getFiles returns 2 unique Type__c values", () => {
+    const element = createElement("c-file-table-container", {
+      is: FileTableContainer
+    });
+    element.recordId = "12345";
+    document.body.appendChild(element);
+
+    getFilesWireAdapter.emit(mockGetFiles);
+
+    return Promise.resolve().then(() => {
+      const input = element.shadowRoot.querySelector("lightning-input");
+      input.dispatchEvent(
+        new CustomEvent("change", { target: { value: "Test" } })
+      );
+
+      const sections = element.shadowRoot.querySelectorAll(
+        "lightning-accordion-section"
+      );
+      expect(sections.length).toEqual(2);
     });
   });
 });
---
+++ force-app/main/default/lwc/select/__tests__/select.test.js
@@ -0,0 +1,79 @@
+import { createElement } from "lwc";
+import Select from "c/select";
+
+describe("c-select", () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  it("Displays a list of 1 if @api options is not set", () => {
+    const element = createElement("c-select", {
+      is: Select
+    });
+    document.body.appendChild(element);
+
+    const options = element.shadowRoot.querySelectorAll("option");
+    expect(options.length).toEqual(1);
+  });
+
+  it("Displays a list of 4 options if @api options has 3 values", () => {
+    const element = createElement("c-select", {
+      is: Select
+    });
+    element.options = [
+      {
+        value: "Option 1",
+        label: "Option 1"
+      },
+      {
+        value: "Option 2",
+        label: "Option 2"
+      },
+      {
+        value: "Option 3",
+        label: "Option 3"
+      }
+    ];
+    document.body.appendChild(element);
+
+    const options = element.shadowRoot.querySelectorAll("option");
+    expect(options.length).toEqual(4);
+  });
+
+  it("Dispatches an onchange event when an option is selected", () => {
+    const element = createElement("c-select", {
+      is: Select
+    });
+    element.options = [
+      {
+        value: "Option 1",
+        label: "Option 1"
+      },
+      {
+        value: "Option 2",
+        label: "Option 2"
+      },
+      {
+        value: "Option 3",
+        label: "Option 3"
+      }
+    ];
+    document.body.appendChild(element);
+
+    const handler = jest.fn();
+    element.addEventListener("change", handler);
+
+    const select = element.shadowRoot.querySelector("select");
+    select.dataset.id = "1";
+    select.value = "Option 1";
+    select.dispatchEvent(new CustomEvent("change"));
+
+    return Promise.resolve().then(() => {
+      expect(handler).toHaveBeenCalled();
+      expect(handler.mock.calls[0][0].detail.value).toEqual("Option 1");
+    });
+  });
+});
