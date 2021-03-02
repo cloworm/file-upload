1ab9a44c37f96ef47dfb4b1e7278690dc7f3f38d
Chloe Echikson <Chloe.Echikson@gmail.com>
2021-03-02 09:19:10 -0500

    Formatted upload component error message

--- force-app/main/default/lwc/fileUpload/fileUpload.js
+++ force-app/main/default/lwc/fileUpload/fileUpload.js
@@ -1,192 +1,11 @@
 import { LightningElement, api } from "lwc";
 import getVersionInfo from "@salesforce/apex/FileUploadController.getVersionInfo";
 
-const extensionToMimeType = {
-  csv: "text/csv",
-  doc: "application/msword",
-  docx:
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  dot: "application/msword",
-  ics: "text/calendar",
-  mdb: "application/vnd.ms-access",
-  pdf: "application/pdf",
-  pps: "application/vnd.ms-powerpoint",
-  ppsx:
-    "application/vnd.openxmlformats-officedocument.presentationml.slideshow",
-  ppt: "application/vnd.ms-powerpoint",
-  pptx:
-    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-  rtf: "application/rtf",
-  sxc: "application/vnd.sun.xml.calc",
-  sxi: "application/vnd.sun.xml.impress",
-  sxw: "application/vnd.sun.xml.writer",
-  txt: "text/plain",
-  xls: "application/vnd.ms-excel",
-  xlsx: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-  bmp: "image/bmp",
-  gif: "image/gif",
-  jpeg: "image/jpeg",
-  jpg: "image/jpeg",
-  png: "image/png",
-  tif: "image/tiff",
-  tiff: "image/tiff",
-  vsd: "application/vnd.visio",
-  mp3: "audio/mpeg",
-  ogg: "application/ogg",
-  wav: "audio/wav",
-  mov: "video/quicktime",
-  mpeg: "video/mpeg",
-  mpg: "video/mpeg",
-  zip: "application/zip",
-  css: "text/css",
-  htm: "text/html",
-  html: "text/html",
-  js: "text/javascript",
-  xml: "	text/xml",
-  xsl: "text/xsl",
-  xslt: "application/xslt+xml"
-};
-
-const BASE_STYLES = `
-.slds-file-selector.slds-file-selector_files {
-  width: 100%;
-  height: 165px;
-}
-
-.slds-file-selector__dropzone {
-  height: 100%;
-  width: 100%;
-  background-color: #fafcfe;
-  background-image: repeating-linear-gradient(
-    0deg,
-    #5eb4ff,
-    #5eb4ff 7px,
-    transparent 7px,
-    transparent 14px,
-    #5eb4ff 14px
-  ),
-  repeating-linear-gradient(
-    90deg,
-    #5eb4ff,
-    #5eb4ff 7px,
-    transparent 7px,
-    transparent 14px,
-    #5eb4ff 14px
-  ),
-  repeating-linear-gradient(
-    180deg,
-    #5eb4ff,
-    #5eb4ff 7px,
-    transparent 7px,
-    transparent 14px,
-    #5eb4ff 14px
-  ),
-  repeating-linear-gradient(
-    270deg,
-    #5eb4ff,
-    #5eb4ff 7px,
-    transparent 7px,
-    transparent 14px,
-    #5eb4ff 14px
-  );
-  background-size: 1px calc(100% + 14px), calc(100% + 14px) 1px,
-    1px calc(100% + 14px), calc(100% + 14px) 1px;
-  background-position: 0 0, 0 0, 100% 0, 0 100%;
-  background-repeat: no-repeat;
-  border: none;
-}
-
-.slds-file-selector__body {
-  height: 165px;
-  justify-content: center;
-}
-
-slot[interop-primitiveFileDroppableZone_primitiveFileDroppableZone] {
-  width: 100%;
-}
-`;
-
-const HOVER_STYLES = `
-.slds-file-selector.slds-file-selector_files {
-  width: 100%;
-  height: 165px;
-}
-
-.slds-file-selector__dropzone {
-  height: 100%;
-  width: 100%;
-  background-color: #fafcfe;
-  background-image: repeating-linear-gradient(
-    0deg,
-    #5eb4ff,
-    #5eb4ff 7px,
-    transparent 7px,
-    transparent 14px,
-    #5eb4ff 14px
-  ),
-  repeating-linear-gradient(
-    90deg,
-    #5eb4ff,
-    #5eb4ff 7px,
-    transparent 7px,
-    transparent 14px,
-    #5eb4ff 14px
-  ),
-  repeating-linear-gradient(
-    180deg,
-    #5eb4ff,
-    #5eb4ff 7px,
-    transparent 7px,
-    transparent 14px,
-    #5eb4ff 14px
-  ),
-  repeating-linear-gradient(
-    270deg,
-    #5eb4ff,
-    #5eb4ff 7px,
-    transparent 7px,
-    transparent 14px,
-    #5eb4ff 14px
-  );
-  background-size: 1px calc(100% + 14px), calc(100% + 14px) 1px,
-    1px calc(100% + 14px), calc(100% + 14px) 1px;
-  background-position: 0 0, 0 0, 100% 0, 0 100%;
-  background-repeat: no-repeat;
-  border: none;
-  animation: borderAnimation 0.5s infinite linear;
-}
-
-.slds-file-selector__body {
-  height: 165px;
-  justify-content: center;
-}
-
-slot[interop-primitiveFileDroppableZone_primitiveFileDroppableZone] {
-  width: 100%;
-}
-
-@keyframes borderAnimation {
-  from {
-    background-position: 0 0, -14px 0, 100% -14px, 0 100%;
-  }
-  to {
-    background-position: 0 -14px, 0 0, 100% 0, -14px 100%;
-  }
-}
-`;
-
-// TO DO check file size
-// TO DO send data in chunks
 export default class FileUpload extends LightningElement {
   @api recordId;
-
-  // Component Properties
-  // TO DO convert to getter & setter, getter returns an array
   @api fileExtensions;
-  hasRendered;
 
-  uploadIcon = "utility:open_folder";
-  extensionToMimeType = extensionToMimeType;
+  hasRendered;
   isDragging = false;
 
   renderedCallback() {
@@ -221,15 +40,21 @@ export default class FileUpload extends LightningElement {
       .uploader slot[interop-primitiveFileDroppableZone_primitiveFileDroppableZone] {
         width: 100%;
       }
+
+      .uploader .slds-has-error .slds-form-element__help {
+        text-align: center;
+        position: relative;
+        top: -30px;
+      }
     `;
     this.template.querySelector(".uploader").appendChild(style);
   }
 
   get hasInvalidConfig() {
-    return [...new Set(this.fileExtensions.split(","))].find((extension) => {
-      let key = extension.replace(".", "").toLowerCase();
-      return !this.extensionToMimeType[key];
-    });
+    return false;
+    // return [...new Set(this.fileExtensions.split(","))].find((extension) => {
+    //   // let key = extension.replace(".", "").toLowerCase();
+    // });
   }
 
   // Get list of extensions set by admin in component properties
@@ -238,61 +63,6 @@ export default class FileUpload extends LightningElement {
     const uniqueExtensions = [...new Set(this.fileExtensions.split(","))];
 
     return uniqueExtensions.sort().join(", ");
-  }
-
-  // Use fileExtensions to get list of allowed mime types
-  get acceptedMimeTypes() {
-    if (!this.fileExtensions) return "";
-
-    return this.fileExtensions
-      .split(",")
-      .map((extension) => {
-        let key = extension.replace(".", "");
-        return this.extensionToMimeType[key];
-      })
-      .join(",");
-  }
-
-  handleInputChange(event) {
-    [...event.target.files].forEach((file) => this.processFile(file));
-  }
-
-  // Read file contents and convert to base64 encoded string
-  processFile(file) {
-    const reader = new FileReader();
-
-    reader.onload = () => {
-      var base64 = reader.result.split(",")[1];
-      const fileData = {
-        id: this.uniqueID(),
-        filename: file.name,
-        base64,
-        recordId: this.recordId,
-        type: file.type,
-        ContentDocumentId: null,
-        size: file.size,
-        error: null
-      };
-
-      // Check if file extension is allowed
-      const extension = this.getFileExtension(file.name);
-      if (
-        !file.type ||
-        !this.fileExtensions.split(",").includes("." + extension)
-      ) {
-        // Display error if not an allowed type
-        fileData.error = "File extension is not allowed";
-      }
-
-      const evt = new CustomEvent("load", { detail: fileData });
-      this.dispatchEvent(evt);
-    };
-
-    reader.readAsDataURL(file);
-  }
-
-  getFileExtension(filename) {
-    return filename.split(".").pop();
   }
 
   // Apply dropzone hover styles ondragenter
@@ -325,36 +95,12 @@ export default class FileUpload extends LightningElement {
     console.log("drop");
 
     this.hideDropzoneHover();
-
-    // [...event.dataTransfer.files].forEach((file) => {
-    //   this.processFile(file);
-    // });
-    // let el = this.template.querySelector("lightning-file-upload");
-    // console.log("el", JSON.parse(JSON.stringify(el)));
-    // let evt = new Event("drop", {
-    //   event: { dataTransfer: { files: event.dataTransfer.files } }
-    // });
-    // el.dispatchEvent(evt);
-  }
-
-  uniqueID() {
-    // Math.random should be unique because of its seeding algorithm.
-    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-    // after the decimal.
-    return "_" + Math.random().toString(36).substr(2, 9);
   }
 
   // Apply dropzone hover styles
   showDropzoneHover() {
     const dropzone = this.template.querySelector("[data-id=dropzone]");
     dropzone.classList.add("box-animate");
-    // this.uploadIcon = "utility:opened_folder";
-    // let uploader = this.template.querySelector(".uploader");
-    // console.log("uploader!!", JSON.parse(JSON.stringify(uploader)));
-    // let children = uploader.children;
-    // console.log("children", JSON.parse(JSON.stringify(children)));
-    // let style = this.template.querySelector("style");
-    // console.log("style", JSON.parse(JSON.stringify(style)));
     this.isDragging = true;
   }
 
@@ -362,7 +108,6 @@ export default class FileUpload extends LightningElement {
   hideDropzoneHover() {
     const dropzone = this.template.querySelector("[data-id=dropzone]");
     dropzone.classList.remove("box-animate");
-    // this.uploadIcon = "utility:open_folder";
     this.isDragging = false;
   }
 
--- force-app/main/default/lwc/uploadFilesByType/uploadFilesByType.js
+++ force-app/main/default/lwc/uploadFilesByType/uploadFilesByType.js
@@ -1,12 +1,8 @@
 import { LightningElement, api, track, wire } from "lwc";
-import uploadFile from "@salesforce/apex/FileUploadController.uploadFile";
-import finalizeVersion from "@salesforce/apex/FileUploadController.finalizeVersion";
 import updateVersionTypes from "@salesforce/apex/FileUploadController.updateVersionTypes";
 import { getPicklistValues, getObjectInfo } from "lightning/uiObjectInfoApi";
 import CONTENT_VERSION_OBJECT from "@salesforce/schema/ContentVersion";
 import TYPE_FIELD from "@salesforce/schema/ContentVersion.Type__c";
-
-const CHUNK_SIZE = 750000;
 
 export default class UploadFilesByType extends LightningElement {
   @api recordId;
@@ -39,77 +35,16 @@ export default class UploadFilesByType extends LightningElement {
     }
   }
 
-  handleLoad(event) {
-    const file = event.detail;
-    this.fileQueue.push({ ...file });
-
-    this.handleOpenModal();
-  }
-
-  // Create a ContentVersion and attach file to the given recordId using the provided base64 and filename
-  handleUpload(file) {
-    let fromPos = 0;
-    let toPos = Math.min(file.base64.length, fromPos + CHUNK_SIZE);
-
-    this.filesUploaded.push(file);
-
-    this.uploadChunk(file, fromPos, toPos);
-  }
-
-  uploadChunk(file, fromPos, toPos) {
-    const chunk = file.base64.substring(fromPos, toPos);
-
-    return uploadFile({
-      ...file,
-      versionId: file.Id,
-      chunk
-    })
-      .then(async (result) => {
-        file.Id = result.Id;
-        fromPos = toPos;
-        toPos = Math.min(file.base64.length, fromPos + CHUNK_SIZE);
-
-        if (fromPos < toPos) {
-          this.uploadChunk(file, fromPos, toPos);
-        } else {
-          const final = await finalizeVersion({
-            contentVersionId: file.Id,
-            recordId: this.recordId
-          });
-          const idx = this.getFileIdxById(file.id);
-          this.filesUploaded[idx].Id = final.Id;
-          this.filesUploaded[idx].ContentDocumentId = final.ContentDocumentId;
-          this.filesUploaded[idx].FileType = final.FileType;
-
-          if (this.grid) {
-            // Refresh the file grid component
-            this.template.querySelector("c-file-grid-container").refresh();
-          }
-        }
-      })
-      .catch((error) => {
-        console.log("error", error);
-        const idx = this.getFileIdxById(file.id);
-        this.filesUploaded[idx].error = "Server error";
-      });
-  }
-
-  getFileIdxById(id) {
-    return this.filesUploaded.findIndex((file) => file.id === id);
-  }
-
   get hasFiles() {
     return this.filesUploaded && this.filesUploaded.length > 0;
   }
 
   handleOpenModal() {
-    // const modal = this.template.querySelector("c-modal");
     const modal = this.template.querySelector(`[data-id="types"]`);
     modal.show();
   }
 
   handleCancelModal() {
-    // const modal = this.template.querySelector("c-modal");
     const modal = this.template.querySelector(`[data-id="types"]`);
     modal.hide();
 
@@ -118,20 +53,11 @@ export default class UploadFilesByType extends LightningElement {
 
   handleCloseModal() {
     // validate form
-
     if (!this.formValid) return;
 
-    // const modal = this.template.querySelector("c-modal");
     const modal = this.template.querySelector(`[data-id="types"]`);
     modal.hide();
 
-    // upload files
-    // this.fileQueue.forEach((file) => {
-    //   if (file.error) return;
-
-    //   // this.handleTypeUpdate()
-    //   // this.handleUpload(file);
-    // });
     this.handleTypeUpdate(this.fileQueue)
       .then((response) => {
         console.log("response", response);
@@ -163,11 +89,8 @@ export default class UploadFilesByType extends LightningElement {
   handleUploaded(event) {
     const files = event.detail.files;
     console.log("files", JSON.parse(JSON.stringify(files)));
-    console.log("handleUploaded", files.length);
 
-    // this.fileQueue.push(...files);
     this.fileQueue = files;
-
     this.handleOpenModal();
   }
 
