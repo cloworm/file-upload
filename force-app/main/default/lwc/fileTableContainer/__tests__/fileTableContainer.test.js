d0c42cdf299f18deafb61d71dcd576919f6155e8
Chloe Echikson <Chloe.Echikson@gmail.com>
2021-03-02 14:13:53 -0500

    testing fileTableContainer

--- force-app/main/default/lwc/fileTableContainer/__tests__/fileTableContainer.test.js
+++ force-app/main/default/lwc/fileTableContainer/__tests__/fileTableContainer.test.js
@@ -1,5 +1,9 @@
 import { createElement } from "lwc";
 import FileGridContainer from "c/fileTableContainer";
+import getFiles from "@salesforce/apex/FileTableController.getFiles";
+const mockGetFiles = require("./data/getFiles.json");
+import { registerLdsTestWireAdapter } from "@salesforce/sfdx-lwc-jest";
+const getFilesWireAdapter = registerLdsTestWireAdapter(getFiles);
 
 describe("c-file-table-container", () => {
   afterEach(() => {
@@ -18,5 +22,20 @@ describe("c-file-table-container", () => {
 
     const message = element.shadowRoot.querySelector("h2");
     expect(message.textContent).toContain("All Files (0)");
+  });
+
+  it("Displays the file count in a header tag when there are 2 files", () => {
+    const element = createElement("c-file-table-container", {
+      is: FileGridContainer
+    });
+    element.recordId = "12345";
+    document.body.appendChild(element);
+
+    getFilesWireAdapter.emit(mockGetFiles);
+
+    return Promise.resolve().then(() => {
+      const message = element.shadowRoot.querySelector("h2");
+      expect(message.textContent).toContain("All Files (2)");
+    });
   });
 });
